2f40001f11d2f3124b188ce25c4265460c015538
Kaspar123 <kasparhaavajoe@gmail.com>
2020-01-19 19:24:17 +0200

    added beamng data capture

---
+++ bottom_up_saliency/IK_model/clean.sh
@@ -0,0 +1,7 @@
+rm features/color/*
+rm features/intensity/*
+rm features/orientation/*
+rm conspicuity_maps/*
+rm pyramids/*
+rm output/*
+rm -r __pycache__
--- saliency_model_1/clean.sh
+++
@@ -1,7 +0,0 @@
-rm features/color/*
-rm features/intensity/*
-rm features/orientation/*
-rm conspicuity_maps/*
-rm pyramids/*
-rm output/*
-rm -r __pycache__
